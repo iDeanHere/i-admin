c61cc3c0d9c17c2fa66b65c94da912330bbe9c65
iDean <oletsgame@qq.com>
2021-03-13 11:57:54 +0800

    第一次运行测试
    安装 element-plus 和 normalize.css 并正确引入。

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus)
+  .mount('#app')
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-button>Test</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
