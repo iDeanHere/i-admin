ace15b47b401aae4bcf73e1d149a62717b0a465d
iDean <oletsgame@qq.com>
2021-03-13 16:31:58 +0800

    在 Sidebar 中先显示一层菜单项

--- src/layout/Sidebar/index.vue
+++ src/layout/Sidebar/index.vue
@@ -1,20 +1,67 @@
 <template>
   <div :class="{ 'has-logo': showLogo }">
     <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu></el-menu>
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :collapse-transition="false"
+        :background-color="variables.menuBgColor"
+        :text-color="variables.menuTextColor"
+        :active-text-color="variables.menuActiveTextColor"
+        :unique-opened="false"
+        mode="vertical"
+      >
+        <el-menu-item v-for="route in routes" :key="route.path">{{
+          getRouteTitle(route)
+        }}</el-menu-item>
+      </el-menu>
     </el-scrollbar>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, reactive } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 const Sidebar = defineComponent({
   name: 'Sidebar',
   setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const variables = reactive({
+      menuBgColor: '#304156',
+      menuTextColor: '#bfcbd9',
+      menuActiveTextColor: '#f4f4f5'
+    })
     const showLogo = computed(() => {
+      return true
+    })
+    const activeMenu = computed(() => {
+      const { meta, path } = route
+      return meta.activeMenu !== null ? meta.activeMenu : path
+    })
+    const isCollapse = computed(() => {
       return false
     })
-    return { showLogo }
+    const routes = computed(() => {
+      return router.options.routes
+    })
+    function getRouteTitle(item: any) {
+      if (item.children[0]) {
+        const child = item.children[0]
+        if (child.meta && child.meta.title) {
+          return child.meta.title
+        }
+      }
+    }
+    return {
+      routes,
+      showLogo,
+      variables,
+      activeMenu,
+      isCollapse,
+      // func
+      getRouteTitle
+    }
   }
 })
 export default Sidebar
