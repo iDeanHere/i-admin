62d6a9b2102b5c718b3d5e981140a24c900f9942
iDean <oletsgame@qq.com>
2021-03-13 17:21:11 +0800

    添加 svg 图标
    从原项目中将 .svg 文件直接拷贝过来。

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    '@typescript-eslint/no-var-requires': 0
   }
 }
