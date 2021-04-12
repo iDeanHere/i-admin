b3e5712b78a500c8895c4e195560c03177b921df
iDean <oletsgame@qq.com>
2021-04-12 18:04:35 +0800

    添加简单的 vuex 测试内容

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    count: 0
   },
   mutations: {
+    increment(state) {
+      state.count++
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,12 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import store from '@/store'
+export default {
+  created() {
+    store.commit('increment')
+    console.log('count: ' + store.state.count)
+  }
+}
+</script>
