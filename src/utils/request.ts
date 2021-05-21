40092e7f81e9a724be046ba063d51cf9486d26f1
iDean <oletsgame@qq.com>
2021-05-21 18:02:36 +0800

    更新 request 工具类

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { store } from '@/store'
+import { getToken } from '@/utils/cookies'
+import { ElMessage, ElMessageBox } from 'element-plus'
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_BASE_API_URI,
@@ -7,6 +10,9 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   config => {
+    if (store.getters.token) {
+      config.headers['X-Token'] = getToken()
+    }
     return config
   },
   error => {
@@ -19,10 +25,49 @@ instance.interceptors.response.use(
   response => {
     const result = response.data
     console.log(result)
+    if (!result.code) {
+      return Promise.reject(
+        new Error('Error(' + response.status + '): ' + response.statusText)
+      )
+    }
+    if (result.code !== 20000) {
+      ElMessage({
+        message: result.msg || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+      if ([50008, 50012, 50014].includes(result.code)) {
+        ElMessageBox.confirm(
+          'You have been logged out, you can cancel to stay on this page, or log in again',
+          'Confirm logout',
+          {
+            confirmButtonText: 'Re-Login',
+            cancelButtonText: 'Cancel',
+            type: 'warning'
+          }
+        ).then(() => {
+          store.dispatch('user/clearToken').then(() => {
+            location.reload()
+          })
+        })
+      }
+      return Promise.reject(
+        new Error(
+          'Error(' + result.code + '): ' + result.msg
+            ? result.msg
+            : 'no message'
+        )
+      )
+    }
     return result
   },
   error => {
     console.error(error)
+    ElMessage({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
     return Promise.reject(error)
   }
 )
